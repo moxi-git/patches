bfb4a97c5985612da57ac40a6b81210dfa2d078c
moxi-git <moxi-git>
2025-10-12 01:14:59 +0200

    added apply all axion patches bash script Signed-off-by: moxi-git <207268292+moxi-git@users.noreply.github.com>

---
+++ apply-patches_axion.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+# move patches to expected dirs
+mv "/home/moxi/patches/rom_patches/Axion/0001-made-for-axionasop-mk-files-and-fixed-overlay.patch" "/home/moxi/my-roms/axion/device/samsung/a53x/"
+mv "/home/moxi/patches/rom_patches/Axion/0002-modified-genfs_contexts-to-work-with-axionAOSP.patch" "/home/moxi/my-roms/axion/device/samsung/s5e8825-common/sepolicy/vendor/"
+
+cd "/home/moxi/my-roms/axion/device/samsung/a53x/"
+
+if [ -d ".git/rebase-apply" ]; then
+    echo "abort"
+    git am --abort
+fi
+
+echo "apply 0001-made-for-axionasop-mk-files-and-fixed-overlay.patch | patch"
+git am 0001-made-for-axionasop-mk-files-and-fixed-overlay.patch
+
+sleep 1
+
+cd "/home/moxi/my-roms/axion/device/samsung/s5e8825-common/sepolicy/vendor"
+
+if [ -d ".git/rebase-apply" ]; then
+    echo "abort"
+    git am --abort
+fi
+
+echo "apply 0002-modified-genfs_contexts-to-work-with-axionAOSP.patch | patch"
+git am 0002-modified-genfs_contexts-to-work-with-axionAOSP.patch
